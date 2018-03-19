40a66890ffe928c246fb20c105b3e8a9dcae15bd
ecisneros24 <elthoncisneros01@gmail.com>
2018-03-19 16:58:18 -0700

    Version 2

--- room3.c
+++ room3.c
@@ -1,5 +1,6 @@
 //Elthon Cisneros
 //Room 3.c
+//3/19/18
 
 
 #include <stdio.h>
@@ -9,5 +10,11 @@ int main (void)
 {
 
 	printf("This is Room 3.\n ");
+	printf ("You walk in and see 4 cards facing down on a table...\n");
+	printf ("You may choose 1 card to determine your fate:\n");
+
+
+
+	return 0;
 
 }
