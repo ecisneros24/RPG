6c5e9dae7f2d948ea877c6fdc5a714a82ab1f123
ecisneros24 <elthoncisneros01@gmail.com>
2018-03-19 17:11:35 -0700

    Version 3

--- room3.c
+++ room3.c
@@ -9,12 +9,24 @@
 int main (void)
 {
 
+	int input;
 	printf("This is Room 3.\n ");
 	printf ("You walk in and see 4 cards facing down on a table...\n");
 	printf ("You may choose 1 card to determine your fate:\n");
+	scanf ("%d", &input);
 
-
-
+	switch (input)
+	{
+		case 1:
+			printf("You die.\n");
+			break;
+		case 2:
+			printf ("You survive.\n");
+			break;
+			// you may choose another card
+		default:
+			break;
+	
+	}
 	return 0;
-
 }
